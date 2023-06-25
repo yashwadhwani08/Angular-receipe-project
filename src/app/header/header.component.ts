35ad1f1fb1f7fddd58fb31002523bf2bf9cf8569
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-06-25 17:11:58 +0530

    Generated the necessary components under the relevant folder structure

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: 'app-header',
+    styles: [],
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent{
+
+}
---
+++ src/app/receipes/receipe-detail/receipe-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-receipe-detail',
+  templateUrl: './receipe-detail.component.html',
+  styleUrls: ['./receipe-detail.component.css']
+})
+export class ReceipesDetailComponent {
+
+}
