6eb6ee5759868958faf287742d2524665cdce96c
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-06-25 17:12:52 +0530

    Updated declarations array with the generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ReceipesComponent } from './receipes/receipes.component';
+import { ReceipesListComponent } from './receipes/receipe-list/receipe-list.component';
+import { ReceipesDetailComponent } from './receipes/receipe-detail/receipe-detail.component';
+import { ReceipeItemComponent } from './receipes/receipe-list/receipe-item/receipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, ReceipesComponent, ReceipesListComponent, ReceipesDetailComponent, ReceipeItemComponent, ShoppingListComponent, ShoppingEditComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
