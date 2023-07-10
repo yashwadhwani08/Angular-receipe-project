6e63c79266ce9ac7926b906b0ab2f0d537fa8131
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-07-10 07:14:38 +0530

    Using the model we defined, the data-type of property 'receipes' set to array of model 'Receipe'

--- src/app/receipes/receipe-list/receipe-list.component.ts
+++ src/app/receipes/receipe-list/receipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Receipe } from '../receipe.model';
 
 @Component({
   selector: 'app-receipe-list',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./receipe-list.component.css']
 })
 export class ReceipesListComponent {
+  receipes: Receipe[] = [
+    new Receipe('A test Reciepe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVh0JUzd3dQS45AyORhwOqMVQpXF9qBzghTjjbAmgyu-8JE5TKRdgxjBeo152R2F7Z3wFhq9-nrNM&usqp=CAU&ec=48600112'),
+    new Receipe('A test Reciepe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVh0JUzd3dQS45AyORhwOqMVQpXF9qBzghTjjbAmgyu-8JE5TKRdgxjBeo152R2F7Z3wFhq9-nrNM&usqp=CAU&ec=48600112')
+  ];
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
 
 }
