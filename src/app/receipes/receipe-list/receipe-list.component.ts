1cf11fb236569f078cd586c55f7b068d17c57ff0
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-10-18 09:01:22 +0530

    Temporary Solution for implementing the feature that receipe detail should hold the data of the selected receipe, passing data from child-component (of receipe-list) 'receipe-item' to parent-component (of receipe-list) 'receipes'

--- src/app/receipes/receipe-list/receipe-list.component.ts
+++ src/app/receipes/receipe-list/receipe-list.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Receipe } from '../receipe.model';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Receipe } from 'src/app/receipes/receipe.model';
 
 @Component({
   selector: 'app-receipe-list',
@@ -7,6 +7,8 @@ import { Receipe } from '../receipe.model';
   styleUrls: ['./receipe-list.component.css']
 })
 export class ReceipesListComponent {
+  @Output() receipeWasSelected = new EventEmitter<Receipe>();
+
   receipes: Receipe[] = [
     new Receipe('A test Reciepe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVh0JUzd3dQS45AyORhwOqMVQpXF9qBzghTjjbAmgyu-8JE5TKRdgxjBeo152R2F7Z3wFhq9-nrNM&usqp=CAU&ec=48600112'),
     new Receipe('A test Reciepe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVh0JUzd3dQS45AyORhwOqMVQpXF9qBzghTjjbAmgyu-8JE5TKRdgxjBeo152R2F7Z3wFhq9-nrNM&usqp=CAU&ec=48600112')
@@ -15,7 +17,13 @@ export class ReceipesListComponent {
   constructor() {}
 
   ngOnInit() {
+  }
 
+  onReceipeSelected(selectedReceipe: Receipe,event: any){
+    console.log("showDetailedReceipe function wworking!");
+    console.log(typeof(event));
+    console.log(event);
+    this.receipeWasSelected.emit(selectedReceipe);
   }
 
 }
