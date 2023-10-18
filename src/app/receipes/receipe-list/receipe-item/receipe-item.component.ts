16886c2bc598fc82b256e33772cb8bc83f3f0946
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-10-18 08:26:15 +0530

    Custom property binding created 'receipe' for 'receipe-item' component

--- src/app/receipes/receipe-list/receipe-item/receipe-item.component.ts
+++ src/app/receipes/receipe-list/receipe-item/receipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Receipe } from 'src/app/receipes/receipe.model';
 
 @Component({
   selector: 'app-receipe-item',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./receipe-item.component.css']
 })
 export class ReceipeItemComponent {
+  @Input() 'receipe': Receipe;
+  receipes: any[] = [];
 
 }
