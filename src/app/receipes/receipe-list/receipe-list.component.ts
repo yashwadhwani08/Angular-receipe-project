7b4e4683a436f6df7831c18541da290c53a701a3
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-10-18 19:48:23 +0530

    changing the 'receipe-name' to view difference on rendering

--- src/app/receipes/receipe-list/receipe-list.component.ts
+++ src/app/receipes/receipe-list/receipe-list.component.ts
@@ -11,7 +11,7 @@ export class ReceipesListComponent {
 
   receipes: Receipe[] = [
     new Receipe('A test Reciepe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVh0JUzd3dQS45AyORhwOqMVQpXF9qBzghTjjbAmgyu-8JE5TKRdgxjBeo152R2F7Z3wFhq9-nrNM&usqp=CAU&ec=48600112'),
-    new Receipe('A test Reciepe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVh0JUzd3dQS45AyORhwOqMVQpXF9qBzghTjjbAmgyu-8JE5TKRdgxjBeo152R2F7Z3wFhq9-nrNM&usqp=CAU&ec=48600112')
+    new Receipe('Another test Reciepe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVh0JUzd3dQS45AyORhwOqMVQpXF9qBzghTjjbAmgyu-8JE5TKRdgxjBeo152R2F7Z3wFhq9-nrNM&usqp=CAU&ec=48600112')
   ];
 
   constructor() {}
