9c7a322404291a2a8dd34480933d015628175177
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-10-19 22:54:11 +0530

    Implementing the feature of adding to the 'ingredients' array via template-refernce (accessed through ViewChild) and custom event binding

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, ViewChild, Output, ElementRef } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+  @Output() addIngredient = new EventEmitter<Ingredient>();
+  // newIngredient: Ingredient = {name:'', amount:null};
+
+  onAddClick(event: PointerEvent){
+    console.log(typeof(event));
+    console.log(event);
+    const newIngredientName = this.nameInput.nativeElement.value;
+    const newIngredientAmount = this.amountInput.nativeElement.value;
+    const newIngredient = new Ingredient(newIngredientName, newIngredientAmount)
+    this.addIngredient.emit(newIngredient);
+  }
 
 }
