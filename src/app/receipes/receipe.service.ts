e2dc2df6b9f5d41e86a0fe5fc857b0299e4976b3
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-11-01 08:57:03 +0530

    created receipe and shopping-list services

---
+++ src/app/receipes/receipe.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Receipe } from './receipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReceipeService {
+  private receipes: Receipe[] = [
+    new Receipe('A test Reciepe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVh0JUzd3dQS45AyORhwOqMVQpXF9qBzghTjjbAmgyu-8JE5TKRdgxjBeo152R2F7Z3wFhq9-nrNM&usqp=CAU&ec=48600112'),
+    new Receipe('Another test Reciepe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVh0JUzd3dQS45AyORhwOqMVQpXF9qBzghTjjbAmgyu-8JE5TKRdgxjBeo152R2F7Z3wFhq9-nrNM&usqp=CAU&ec=48600112')
+  ];
+
+  getReceipes(){
+    // return this.receipes; // this way we would be passig the actual array, typical JS behaviour
+
+    return this.receipes.slice(); //This returns a copy of array not the actual array!
+  }
+
+
+
+  constructor() { }
+}
