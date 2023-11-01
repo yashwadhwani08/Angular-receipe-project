67a6030bc67656fc7b4a1015a4b093f85eb9545e
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-11-01 08:58:46 +0530

    using service to access list of available receipes in the service-array

--- src/app/receipes/receipe-list/receipe-list.component.ts
+++ src/app/receipes/receipe-list/receipe-list.component.ts
@@ -1,29 +1,27 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Receipe } from 'src/app/receipes/receipe.model';
+import { ReceipeService } from '../receipe.service';
 
 @Component({
   selector: 'app-receipe-list',
   templateUrl: './receipe-list.component.html',
-  styleUrls: ['./receipe-list.component.css']
+  styleUrls: ['./receipe-list.component.css'],
 })
 export class ReceipesListComponent {
   @Output() receipeWasSelected = new EventEmitter<Receipe>();
 
-  receipes: Receipe[] = [
-    new Receipe('A test Reciepe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVh0JUzd3dQS45AyORhwOqMVQpXF9qBzghTjjbAmgyu-8JE5TKRdgxjBeo152R2F7Z3wFhq9-nrNM&usqp=CAU&ec=48600112'),
-    new Receipe('Another test Reciepe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVh0JUzd3dQS45AyORhwOqMVQpXF9qBzghTjjbAmgyu-8JE5TKRdgxjBeo152R2F7Z3wFhq9-nrNM&usqp=CAU&ec=48600112')
-  ];
+  receipes: Receipe[];
 
-  constructor() {}
+  constructor(private receipeService: ReceipeService) {}
 
   ngOnInit() {
+    this.receipes = this.receipeService.getReceipes();
   }
 
-  onReceipeSelected(selectedReceipe: Receipe,event: any){
-    console.log("showDetailedReceipe function wworking!");
-    console.log(typeof(event));
+  onReceipeSelected(selectedReceipe: Receipe, event: any) {
+    console.log('showDetailedReceipe function wworking!');
+    console.log(typeof event);
     console.log(event);
     this.receipeWasSelected.emit(selectedReceipe);
   }
-
 }
