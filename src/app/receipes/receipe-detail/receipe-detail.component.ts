0f6fcff6fa5d274b68125a2c279b6d431516d492
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-11-02 20:23:51 +0530

    adding functionality to add receipe ingredients to shopping list

--- src/app/receipes/receipe-detail/receipe-detail.component.ts
+++ src/app/receipes/receipe-detail/receipe-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Receipe } from 'src/app/receipes/receipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-receipe-detail',
@@ -8,4 +10,12 @@ import { Receipe } from 'src/app/receipes/receipe.model';
 })
 export class ReceipesDetailComponent {
   @Input() receipe: Receipe
+
+  constructor(private shopppingListService: ShoppingListService){}
+  onClickToShoppingList(){
+    this.receipe.ingredients.map((ingredient: Ingredient)=>{
+      this.shopppingListService.addIngredient(ingredient)
+    })
+  }
+    
 }
